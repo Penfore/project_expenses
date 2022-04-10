4b96d8f3fd39908f81c41deac35de5d05522c92b
Fulvio Leo <fulviokaldas@outlook.com>
2022-04-10 17:09:25 -0300

    Code refactoring
    - Created the transaction_list.dart file, wich contains the Column to show the items.
    - Created the transaction_form.dart file, wich now contains a Card that will be replaced by the Modal to add new expenses.
    - Created the transaction_user.dart file, wich is the file to show the informations of the TransactionList and the TransactionForm.
    - Linked all the calls between the files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import 'models/transaction.dart';
+import 'package:project_expenses/components/transaction_user.dart';
 
 void main(List<String> args) {
   runApp(const ExpensesApp());
@@ -19,16 +17,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-        id: 't1',
-        title: 'Novo tênis de corrida',
-        value: 310.76,
-        date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now())
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,70 +35,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Column(
-                children: _transactions.map((transaction) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        "R\$ ${transaction.value.toStringAsFixed(2)}",
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.title,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        Text(
-                          DateFormat('d/MM/y').format(transaction.date),
-                          style: const TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList()),
-            Card(
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    const TextField(
-                      decoration: InputDecoration(labelText: 'Título'),
-                    ),
-                    const TextField(
-                        decoration: InputDecoration(
-                      labelText: 'Valor R\$',
-                    )),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          child: const Text('Nova transação'),
-                          style: TextButton.styleFrom(primary: Colors.purple),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            )
+            const TransactionUser(),
           ],
         ));
   }
