68c4761343c52f6741be7c9bb1faeba436d360db
Fulvio Leo <fulviokaldas@outlook.com>
2022-04-11 12:17:42 -0300

    App themming and Scrolling funcionality
    	- Using widget SingleChildScrollView to add the scrolling funcionality.
    	- Replacing the main Column from the transaction_list.dart file to a ListViee.
    	- Optimizing the ListView to generate the elements based on demand.
    	- Fixing the keyboard issues.
    	- Created the buttons to open the Modal to add new transactions.
    	- Created the Modal and adding the funcionalities to create new transactions and close the Modal screen after.
    	- Added theme pattern.
    	- Added font assets.
    	- Edited the pubspec.yaml file to declare the used fonts.
    	- Applied the font and theme in the application.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,41 +10,48 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions.map((transaction) {
-      return Card(
-        child: Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                "R\$ ${transaction.value.toStringAsFixed(2)}",
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
+    return SizedBox(
+        height: 300,
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (itemBuilderContext, index) {
+            final transaction = transactions[index];
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 2)),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "R\$ ${transaction.value.toStringAsFixed(2)}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transaction.title,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(
+                        DateFormat('d/MM/y').format(transaction.date),
+                        style: const TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  transaction.title,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  DateFormat('d/MM/y').format(transaction.date),
-                  style: const TextStyle(color: Colors.grey),
-                )
-              ],
-            )
-          ],
-        ),
-      );
-    }).toList());
+            );
+          },
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,97 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:project_expenses/components/transaction_user.dart';
+import 'package:project_expenses/components/transaction_form.dart';
+import 'package:project_expenses/models/transaction.dart';
+
+import 'components/transaction_list.dart';
 
 void main(List<String> args) {
-  runApp(const ExpensesApp());
+  runApp(ExpensesApp());
 }
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key? key}) : super(key: key);
+  ExpensesApp({Key? key}) : super(key: key);
+
+  final ThemeData theme = ThemeData();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
+        home: MyHomePage(),
+        theme: theme.copyWith(
+            colorScheme: theme.colorScheme.copyWith(
+              primary: Colors.purple,
+              secondary: Colors.amber,
+            ),
+            textTheme: theme.textTheme.copyWith(
+                headline6: const TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
+            appBarTheme: const AppBarTheme(
+                titleTextStyle: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ))));
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo tênis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now()),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+        id: Random().nextDouble().toString(),
+        title: title,
+        value: value,
+        date: DateTime.now());
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Despesas Pessoais'),
-        ),
-        body: Column(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+              onPressed: () => _openTransactionFormModal(context),
+              icon: const Icon(Icons.add)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             // ignore: sized_box_for_whitespace
@@ -35,8 +102,15 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            const TransactionUser(),
+            TransactionList(_transactions),
           ],
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
